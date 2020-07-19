c74dfeda134e65dea538a0bf290a0f936d0f3eb0
Admin <>
2020-07-20 00:23:25 +0530

    Accessing global variable.

--- demo.py
+++ demo.py
@@ -1,5 +1,37 @@
-print("Main Master\n")
-print("Hi, from 'Kaoushik'\n ")
-print("Master's Line Added.\n")
-print("Spaces Accepted.\n")
-print("Single Statement\n")
+
+customer = input("Catogery:")
+
+
+class Customer:
+    # globals customer
+    # customer == 'vip'
+    def __init__(self, amount):
+        self.amount = amount
+
+    def diss(self):
+        global customer
+        if customer == 'normal':
+            return self.amount - (self.amount * 0.2)
+
+c = Customer(1000).diss()
+print(c)
+
+
+class VipCustomer(Customer):
+
+    # if customer == 'vip'
+    def __init__(self, amount):
+        self.amount = amount
+
+    def discount(self):
+        global customer
+        # super().diss()
+        # self().__init__(1000)
+        if customer == 'vip':
+            return self.amount - (self.amount * 0.4)
+
+
+v = VipCustomer(1000).discount()
+# v.diss()
+print(v)
+# print(Customer(1000).diss())
