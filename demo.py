41b2693673586782775f20fd32a0244c0ed13463
Admin <>
2020-07-20 23:14:54 +0530

    Added Abstract_Factory file.

--- demo.py
+++ demo.py
@@ -6,32 +6,35 @@ class Customer:
     # globals customer
     # customer == 'vip'
     def __init__(self, amount):
-        self.amount = amount
+        self.amount = 1000
 
     def diss(self):
-        global customer
+        # global customer
         if customer == 'normal':
             return self.amount - (self.amount * 0.2)
 
-c = Customer(1000).diss()
-print(c)
+# c = Customer(1000).diss()
+# print(c)
 
 
 class VipCustomer(Customer):
 
     # if customer == 'vip'
     def __init__(self, amount):
+        # super().__init__(amount)
         self.amount = amount
 
     def discount(self):
-        global customer
+        # global customer
         # super().diss()
-        # self().__init__(1000)
+        # super().__init__(amount)
         if customer == 'vip':
             return self.amount - (self.amount * 0.4)
 
 
 v = VipCustomer(1000).discount()
-# v.diss()
 print(v)
+# print(v.diss())
+# .diss()
+# print(v)
 # print(Customer(1000).diss())
