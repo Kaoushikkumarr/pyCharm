c9176ecd7a807ca16f729b1633b571ed20b59ea8
Admin <>
2020-07-22 01:00:25 +0530

    Added mypy_type checker file.

---
+++ mypy_type.py
@@ -0,0 +1,19 @@
+''' To check the type of declaration\
+    we need to run code of 'terminal' not on 'python console\
+     just type mypy file_name.py on the terminal'''
+
+
+def func(x:int, y:int) -> str:
+    return x + y
+print(func(8,3))
+
+
+def func(x, y) :
+    # return x + y
+    return 'Hi, Kaoushik!'
+print(func(8,5))
+
+
+def func(x, y) -> int:
+    return x/y
+print(func(8,3))
